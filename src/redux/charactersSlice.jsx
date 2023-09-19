2aee6d954468212d65e0fe1d94ff91caff1a3148
Furkan Karakus <furkankarakus01@hotmail.com>
2023-09-19 15:15:55 +0300

    add builder cases

--- src/redux/charactersSlice.jsx
+++ src/redux/charactersSlice.jsx
@@ -7,6 +7,13 @@ export const fetchCharacters = createAsyncThunk('characters/getCharacters', asyn
     return res.data;
 
 })
+
+export const fetchCharDetails = createAsyncThunk('char/getCharDetails', async(char_id) => {
+    const res = await axios.get(
+        `${process.env.REACT_APP_API_BASE_ENDPOINT}/characters/?char_id=${char_id}`
+    );
+    return res.data;
+})
 export const charactersSlice = createSlice({
     name: "characters",
     initialState: {
@@ -17,8 +24,16 @@ export const charactersSlice = createSlice({
         hasNextPage: true,
         item: {},
     },
-    reducers: {},
+    reducers: {
+        clearItem: (state) => {
+            state.item = {};
+        }
+    },
     extraReducers: (builder) => {
+        // get Characters
+        builder.addCase(fetchCharacters.pending, (state) => {
+            state.status = "loading";
+        });
         builder.addCase(fetchCharacters.fulfilled, (state, action) => {
             state.items = [...state.items, ...action.payload];
             state.page += 1;
@@ -26,13 +41,29 @@ export const charactersSlice = createSlice({
             if(action.payload.length < 12){
                 state.hasNextPage = false;
             }
-            
-            
         }) 
+        builder.addCase(fetchCharacters.rejected, (state, action) => {
+            state.status = "failed";
+            state.error = action.error.message;
+        })
+
+        // get item
+        builder.addCase(fetchCharDetails.pending, (state, action => {
+            state.status = "loading";
+
+        }))
+        builder.addCase(fetchCharDetails.fulfilled, (state, action) => {
+            state.item = action.payload[0];
+            state.status = "success";
+        })
+        builder.addCase(fetchCharDetails.rejected, (state, action) => {
+            state.status = "failed";
+            state.error = action.error.message;
+        })
          
     }
 });
 
 
-
+export const { clearItem } = charactersSlice.actions;
 export default charactersSlice.reducer;
